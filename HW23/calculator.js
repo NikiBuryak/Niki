e5a024a8d0187a0e2e51720e404b4b51effced94
NikiBuryak <62623000+NikiBuryak@users.noreply.github.com>
2021-02-28 21:31:13 +0200

    Create calculator.js

---
+++ HW23/calculator.js
@@ -0,0 +1,13 @@
+const add = require('./calc/add')
+const sub = require('./calc/sub')
+const mult = require('./calc/mult')
+const div = require('./calc/div')
+
+
+
+module.exports= {
+    add:add.add,
+    sub:sub.sub,
+    mult:mult.mult,
+    div:div.div,
+} 
