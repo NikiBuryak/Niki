eb9498585e381c3c4b80cd6a497110a217baf279
NikiBuryak <62623000+NikiBuryak@users.noreply.github.com>
2021-03-15 01:37:28 +0200

    Update StudentsView.js

--- src/scripts/StudentsView.js
+++ src/scripts/StudentsView.js
@@ -1,14 +1,16 @@
-const BTN_DELETE_SELECTOR = '.delete'
-const INPUT_SELECTOR = '.input'
-const STUDENTS_LIST_SELECTOR = '.students_list'
-class StudentsView{
+import $ from "jquery";
+import { BTN_DELETE_SELECTOR, INPUT_SELECTOR, STUDENTS_LIST_SELECTOR,STUDENT_SELECTOR} from './configs.js'
+
+
+export default class StudentsView{
+
     constructor(options){
         this._options = options;
         this._$el = this.initList();
         this.list =[];
     }
     initList(){
-        return $(`<table class="students_list"></table>`)
+        return $(`<table class="students-list"></table>`)
         .on('focusout', INPUT_SELECTOR, this.onFoucsOutInput.bind(this))
         .on('click',BTN_DELETE_SELECTOR, this.onDeleteBtnClick.bind(this))
     }
@@ -21,8 +23,7 @@ class StudentsView{
         e.stopPropagation();
         e.target.defaultValue = e.target.value;
         const currentStudent = this.findStudent(e.target);
-        this._options.tune(currentStudent);
-
+        this._options.tune(this.getMarks(currentStudent), currentStudent.dataset.id);
     }
 
     changeValue(value){
@@ -48,7 +49,6 @@ class StudentsView{
     }
     setList(data){
         this.list = data;
-        console.log(this.list);
     }
     renderStudents(item){
         const itemHtml = item.map((elem) => this.generateItem(elem)) 
@@ -62,26 +62,30 @@ class StudentsView{
         const newStudent = this.generateItem(res);
         this.appendTo($(STUDENTS_LIST_SELECTOR), newStudent);
     }
-    appendTo($conteiner, el =this._$el){
-        return $conteiner.append(el);
-    }
+    
     generateItem(item){
         if(!!item.marks){
             return `<tr class="student" data-id = ${item.id}>
                     <td class="name">${item.name}</td>
-                    ${  this.isArray(item.marks),
-                        item.marks.map(element => {
-                        return `<td><input type="number" class="input" id=inp${Math.random()} value="${element}"></td>`
+                    ${
+                        this.isArray(item.marks).map(element => {
+                        return `<td><input type="number" class="input" value="${element}"></td>`
                     })}
                     <td class="delete">X</td>
                 </tr>`
         }
-        }
-        isArray(array){
-            if(!Array.isArray(array)){
-                return [0,0,0,0,0,0,0,0,0,0];
-            }
-        }
+    }
+    isArray(array){
+        if(!Array.isArray(array)){
+            return [0,0,0,0,0,0,0,0,0,0];
+        }else {return array;}
+    }
+    getMarks(student){
+        const marks =[];
+        student = (Object.values( student.querySelectorAll('input')))
+        student.map((el) => marks.push(el.value))
+        return marks;
+    }
 }
 
 
